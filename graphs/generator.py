9a3fec958b139b263f93f1cfca8ff9dd81b18b3d
Pedro Lopes da Silva Mariano <plmariano@ciencias.ulisboa.pt>
2018-05-11 17:19:15 +0100

    First version of graph generator.

---
+++ graphs/generator.py
@@ -0,0 +1,54 @@
+from __future__ import print_function
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
+def generate_short_dumb_bell (n):
+    G = nx.Graph()
+    G.add_node('L')
+    G.add_node('R')
+
+    for s in ['L', 'R']:
+        for i in xrange(n):
+            G.add_node ('{}{}'.format(s, i))
+            G.add_edge(s, '{}{}'.format(s, i))
+            for j in xrange (i + 1, n):
+                G.add_edge ('{}{}'.format (s, i), '{}{}'.format (s, j)) 
+
+    G.add_edge('L', 'R')
+    plt.clf ()
+    nx.draw(G, labels=True)
+    print (nx.to_numpy_matrix(G).astype(int))
+
+def generate_long_dumb_bell (n, pole):
+    G = nx.Graph ()
+    G.add_node ('L')
+    G.add_node ('R')
+    G.add_node ('C')
+    G.add_edge ('L', 'C')
+    G.add_edge ('C', 'R')
+    for s in ['L', 'R']:
+        for i in xrange(n):
+            G.add_node ('{}{}'.format (s, i))
+            G.add_edge (s, '{}{}'.format (s, i))
+            if pole == 'full':
+                for j in xrange (i + 1, n):
+                    G.add_edge ('{}{}'.format (s, i), '{}{}'.format (s, j))
+            elif pole == 'whiskers':
+                pass
+            else:
+                raise 'Unknown pole type'
+    return report (G)
+
+def report (graph):
+    matrix = nx.to_numpy_matrix (graph).astype (int)
+    print ('   case XXX')
+    print ('      N = [')
+    for row in matrix:
+        for c in row.flat:
+            print (c, end = ' ')
+        print (';')
+    print ('      ];')
+    print ('      mds = YYY')
+    return matrix
+
