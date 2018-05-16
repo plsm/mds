249cf051f3ad23639e212f2ddb5eba4eb1365862
Pedro Mariano <mariano.pedro@gmail.com>
2018-05-16 08:39:06 +0100

    Function mdsAirRun also saves the number of times the airpump was on.

--- mdsAirRun.m
+++ mdsAirRun.m
@@ -8,17 +8,20 @@ correct = 0;
 subopt = 0;
 incorrect = 0;
 for indexRepeat = 1 : numberRepeats
-  [vec, pStat] = simulateDomsetAirflowBees (N, n, rho);
+  [vec, pStat, airPump] = simulateDomsetAirflowBees (N, n, rho);
   [cor, sub, inc] = calculateStats (vec, N, mds);
   correct = correct + (cor > 0);
   subopt = subopt + (sub > 0);
   incorrect = incorrect + (inc > 0);
   timestamp = time ();
-  repeatInfo = horzcat ([indexRepeat n rho timestamp (cor > 0) (sub > 0) (inc > 0)], vec (end,:));
+  repeatInfo = horzcat ( ...
+    [indexRepeat n rho timestamp (cor > 0) (sub > 0) (inc > 0)], ...
+    vec (end,:), ...
+    sum (airPump, 1));
   runInfo = [runInfo ; repeatInfo];
 end
 correct_percentage = 100 * correct / numberRepeats;
 suboptimal_percentage = 100 * subopt / numberRepeats;
 incorrect_percentage = 100 * incorrect / numberRepeats;
 filename = sprintf ("results-air-graph_%d.csv", indexGraph);
-cell2csv (filename, mat2cell (runInfo, ones (1, numberRepeats), ones (1, 7 + size (N, 1))));
+cell2csv (filename, mat2cell (runInfo, ones (1, numberRepeats), ones (1, 7 + 2 * size (N, 1))));
